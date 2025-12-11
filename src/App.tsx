9f52e219da1b9113022cd9ff498793789c97af9f
kazumi-sansan <kazumisusumago@gmail.com>
2025-12-12 01:03:20 +0900

    JPGにしてみる

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import PDFBook from './components/PDFBook';
 import './App.css';
 
 function App() {
-  const pdfFiles = Array.from({ length: 24 }, (_, i) => `/${i + 1}.png`);
+  const pdfFiles = Array.from({ length: 24 }, (_, i) => `/${i + 1}.jpg`);
 
   return (
     <div className="App">
